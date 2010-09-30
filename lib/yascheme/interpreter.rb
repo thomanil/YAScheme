96050f3d2d07f0084e216ac4641525dfc76efc0f
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2010-09-30 23:46:39 +0200

    Tweaked names, structure to make more readable for myself. Prepared eval function for quotes etc

--- lib/yascheme/interpreter.rb
+++ lib/yascheme/interpreter.rb
@@ -5,48 +5,38 @@ class Interpreter
     # TODO load and eval "optional, library, extensions" scheme code from here
   end
 
-  def evaluate(code)
+  def run(code)
     tokens = @lexer.tokenize(code)
     ast_tree = @parser.ast_tree(tokens)
-    walk ast_tree
+    eval ast_tree
   end
 
-
-  def walk(node, context={})
+  def eval(node, context={})
     type = node.node_type
     children = node.children
    
     if(type.eql?(:root))
-      children.each { |child| @last_result = walk(child, context) }
+      children.each { |child| @last_result = eval(child, context) }
       @last_result
     elsif(type.eql?(:list))
       function_name = children[0].node_value
-      if(operator? function_name) # TODO do this for nonquoted list
-        arguments = children[1..children.length]
-        call_function(function_name, arguments, context)
-      else # TODO Do this if list is quoted
-        list = []
-        children.each { |child| list.push(walk(child, context)) }
-        list
-      end
+      arguments = children[1..children.length]
+      call_function(function_name, arguments, context)
     elsif(type.eql?(:quote))
-      # TODO supports (quote ...) form, also implement ' form as reader macro
-      # TODO return quoted data
+      # TODO if list, return list as data
+      #  list = []
+      #  children.each { |child| list.push(eval(child, context)) }
+      #  list
+      # TODO else if atom, return atom name
     elsif(type.eql?(:symbol))
-      # TODO get value of referenced variable
-      # TODO if quoted, just get the symbol name
+      # TODO resolve variable binding
       node.literal_value
     elsif(type.eql?(:string) || 
           type.eql?(:number))
       node.literal_value    
     else
-      raise "Walking ast tree, encountered unknown node type '#{node.node_type}'"
+      raise "Evaling ast tree, encountered unknown node type '#{node.node_type}'"
     end
-  end
-
-  # TODO only used for testing until quoting works, remove!
-  def operator?(symbol)
-    symbol[/\A([\+\-\/\*])/]
   end
 
   # TODO support more built-in functions
@@ -54,16 +44,16 @@ class Interpreter
   def call_function(name, argument_atoms, context={})
     case name
     when "+"
-      sum = argument_atoms.map {|atom| walk(atom)}.join("+")
+      sum = argument_atoms.map {|atom| eval(atom)}.join("+")
       return Kernel.eval(sum)
     when "-"
-      sum = argument_atoms.map {|atom| walk(atom)}.join("-")
+      sum = argument_atoms.map {|atom| eval(atom)}.join("-")
       return Kernel.eval(sum)
     when "*"
-      sum = argument_atoms.map {|atom| walk(atom)}.join("*")
+      sum = argument_atoms.map {|atom| eval(atom)}.join("*")
       return Kernel.eval(sum)
     when "/"
-      sum = argument_atoms.map {|atom| walk(atom)}.join("/")
+      sum = argument_atoms.map {|atom| eval(atom)}.join("/")
       return Kernel.eval(sum)
     else
       raise "Function '#{name}' undefined"
--- test/test_interpreter.rb
+++ test/test_interpreter.rb
@@ -12,34 +12,33 @@ class TestInterpreter < Test::Unit::TestCase
  end
 
  def test_eval_symbol
-    assert_equal :symbol,  @interpreter.evaluate("symbol")
+    assert_equal :symbol,  @interpreter.run("symbol")
  end   
 
  def test_eval_number
-    assert_equal 42,  @interpreter.evaluate("42")
+    assert_equal 42,  @interpreter.run("42")
  end  
 
  def test_eval_string
-  assert_equal "this is a string",  @interpreter.evaluate("\"this is a string\"")
+  assert_equal "this is a string",  @interpreter.run("\"this is a string\"")
  end  
 
  def test_eval_list
-   assert_equal [1, 2],  @interpreter.evaluate("(1 2)")
-   assert_equal [1, :two, "three"],  @interpreter.evaluate("(1 two \"three\")")
+   #assert_equal [1, 2],  @interpreter.run("'(1 2)")
+   #assert_equal [1, :two, "three"],  @interpreter.run("'(1 two \"three\")")
  end  
 
  def test_eval_nested_list
-   assert_equal [1, [2, 3]],  @interpreter.evaluate("(1 (2 3))")
+   #assert_equal [1, [2, 3]],  @interpreter.run("'(1 (2 3))")
  end
 
  def test_eval_arithmetic_functions
-   assert_equal 2,  @interpreter.evaluate("(+ 1 1)")
-   assert_equal 3,  @interpreter.evaluate("(+ 1 1 1)")
-   assert_equal 4,  @interpreter.evaluate("(+ (+ 1 1) (+ 1 1))")
-
-   assert_equal 1,  @interpreter.evaluate("(- 4 3)")
-   assert_equal 6,  @interpreter.evaluate("(* 2 3)")
-   assert_equal 5,  @interpreter.evaluate("(/ 15 3)")
+   assert_equal 2,  @interpreter.run("(+ 1 1)")
+   assert_equal 3,  @interpreter.run("(+ 1 1 1)")
+   assert_equal 4,  @interpreter.run("(+ (+ 1 1) (+ 1 1))")
+   assert_equal 1,  @interpreter.run("(- 4 3)")
+   assert_equal 6,  @interpreter.run("(* 2 3)")
+   assert_equal 5,  @interpreter.run("(/ 15 3)")
  end
 
 
