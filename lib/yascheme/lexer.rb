9f6804a628ddcd84755c29b7679a8265ffff3c62
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2010-10-06 19:13:13 +0200

    Included TODO list in README, extracted lexer regexes to constants

--- lib/yascheme/lexer.rb
+++ lib/yascheme/lexer.rb
@@ -5,38 +5,48 @@ class Lexer
   def tokenize(code)
     if(unbalanced_parens? code) then syntax_error("Unbalanced parens") end
     if(unbalanced_quots? code) then syntax_error("Unmatched quotation marks") end
-    
+        
+    WHITESPACE = /\A(\s+)/
+    OPEN_PAREN = /\A\(/
+    CLOSE_PARENT = /\A\)/
+    QUOTE_TICK = /\A\'/
+    COMMENT = /\A(;.*)$/
+    ARITHMETIC_OPERATOR = /\A([\+\-\/\*])/
+    NUMBER = /\A(\d+)/
+    SYMBOL = /\A(\w+)/
+    STRING = /\A([\"].*?[\"])/
+
     @scanner = StringScanner.new(code)
     tokens = []
     pos = 0
-    
+
     while(!finished_tokenizing)
       remaining = code[@scanner.pos, code.length]
       
-      if(whitespace = remaining[/\A(\s+)/])
+      if(whitespace = remaining[WHITESPACE])
         skip_ahead whitespace.length
-      elsif(open_paren = remaining[/\A\(/])
+      elsif(open_paren = remaining[OPEN_PAREN])
         tokens.push [:open_paren]
         skip_ahead open_paren.length
-      elsif(close_paren = remaining[/\A\)/])
+      elsif(close_paren = remaining[CLOSE_PAREN])
         tokens.push [:close_paren]
         skip_ahead close_paren.length
-      elsif(quote_tick = remaining[/\A\'/])
+      elsif(quote_tick = remaining[QUOTE_TICK])
         tokens.push([:quote_tick])
         skip_ahead quote_tick.length
-      elsif(comment = remaining[/\A(;.*)$/])
+      elsif(comment = remaining[COMMENT])
         tokens.push([:comment, comment])
         skip_ahead comment.length
-      elsif(operator = remaining[/\A([\+\-\/\*])/])
+      elsif(operator = remaining[ARITHMETIC_OPERATOR])
         tokens.push([:operator, operator])
         skip_ahead operator.length
-      elsif(number = remaining[/\A(\d+)/])
+      elsif(number = remaining[NUMBER])
         tokens.push([:number, number])
         skip_ahead number.length
-      elsif(symbol = remaining[/\A(\w+)/])
+      elsif(symbol = remaining[SYMBOL])
         tokens.push([:symbol, ":#{symbol}"])
         skip_ahead symbol.length
-      elsif(string = remaining[/\A([\"].*?[\"])/])
+      elsif(string = remaining[STRING])
         tokens.push([:string, string])
         skip_ahead string.length
       else
@@ -70,6 +80,5 @@ class Lexer
   def syntax_error(msg)
     raise "SYNTAX ERROR: #{msg}"
   end
-
 
 end
